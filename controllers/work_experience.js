7aee5cc6af1d63fb2a6e841eac48d65f304ce629
Paul Ndambo <164306210+ndambopaul@users.noreply.github.com>
2024-05-23 10:02:10 +0300

    work experience controllers added

--- controllers/work_experience.js
+++ controllers/work_experience.js
@@ -33,3 +33,70 @@ const getDoctorExperienceRecord = async(req, res) => {
         return res.status(500).send({ error: error.message })
     }
 }
+
+
+const createExperienceRecord = async(req, res) => {
+    const errors = validationResult(req);
+
+    if(!errors.isEmpty()) {
+        return res.status(400).send({ errors: errors.array() })
+    }
+
+    const data = req.body
+
+    try {
+        
+        const experience = await Experience.create(data)
+        if(!experience) return res.status(400).send({ error: "Something went wrong, the experience record could not be created!!" })
+        res.send(experience).status(201)
+
+    } catch (error) {
+        console.log({ error: error.message });
+        return res.status(500).send({ error: error.message })
+    }
+}
+
+
+const updateExperienceRecord = async(req, res) => {
+    const errors = validationResult(req);
+
+    if(!errors.isEmpty()) {
+        return res.status(400).send({ errors: errors.array() })
+    }
+
+    const { body, params: { id } } = req
+
+    try {
+        const experience = await Experience.findByIdAndUpdate(id, { ...body }, { new: true })
+        if(!experience) return res.status(404).send({ error: `Experience record with id: ${id} not found!` })
+        res.send(experience).status(200)
+    } catch (error) {
+        console.log({ error: error.message });
+        return res.status(500).send({ error: error.message })
+    }
+}
+
+const deleteExperienceRecord = async(req, res) => {
+    const errors = validationResult(req);
+
+    if(!errors.isEmpty()) {
+        return res.status(400).send({ errors: errors.array() })
+    }
+
+    try {
+        const experience = await Experience.findByIdAndDelete({ "_id": id })
+        if(!experience) return res.status(404).send({ error: `Experience record with id: ${id} not found!!` })
+        res.send({ message: 'Experience record updated successfully!!' }).status(201)
+    } catch (error) {
+        console.log({ error: error.message });
+        return res.status(500).send({ error: error.message })
+    }
+}
+
+
+module.exports = {
+    getAllWorkExperiences,
+    getDoctorExperiences,
+    getDoctorExperienceRecord,
+    createExperienceRecord
+}
